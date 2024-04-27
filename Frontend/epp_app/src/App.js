3060ba727a01274ed1f2792c62674f1857ba2ed6
Royden Dsouza <roydendsouza3108@gmail.com>
2024-04-27 20:18:43 +0530

    navbar added

--- Frontend/epp_app/src/App.js
+++ Frontend/epp_app/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Signup from "./components/signup/signup";
+import Navbar from "./components/navbar/navbar";
 import Homepage from "./components/homepage/homepage";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Signup />
       {/* <Homepage /> */}
     </div>
---
+++ Frontend/epp_app/src/components/navbar/navbar.js
@@ -0,0 +1,14 @@
+import "./navbar.css";
+import logo from "./logo.png"; // Adjust the path to match the actual location
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src={logo} height={100} alt="Logo" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
