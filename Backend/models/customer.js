bf5de20b9f4678afd97bcff3680962a83c548439
Royden Dsouza <81296538+roydendsouza31@users.noreply.github.com>
2024-05-01 18:34:24 +0530

    updated schema of customer

--- Backend/models/customer.js
+++ Backend/models/customer.js
@@ -5,7 +5,7 @@ const CustomerSchema = new mongoose.Schema({
   surname: { type: String, required: true, minlength: 1 },
   email: { type: String, required: true, minlength: 2 },
   password: { type: String, required: true, minlength: 3 },
-  image: { type: String, default: "" },
+  image: { type: Buffer },
 });
 
 module.exports = mongoose.model("Customer", CustomerSchema);
