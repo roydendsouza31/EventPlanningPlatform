8da84377e6e0964e8998edabd6675457bc7afca1
2342-riddhi <mca.2342@unigoa.ac.in>
2024-05-01 10:35:39 +0530

    lading

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,50 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
+// Middleware
+app.use(cors());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/WebDev', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch((err) => console.error('Error connecting to MongoDB:', err));
+
+
+const testimonialSchema = new mongoose.Schema({
+  clientName: {
+    type: String,
+    required: true
+},
+profilePic: {
+  data: Buffer, // Store image data as a Buffer
+  contentType: String // Store content type of the image
+},
+review: {
+    type: String,
+    required: true
+}
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const Testimonial = mongoose.model('Testimonial', testimonialSchema);
+
+// Route to fetch testimonials
+app.get('/api/testimonials', async (req, res) => {
+  try {
+    const testimonials = await Testimonial.find();
+    res.json(testimonials);
+  } catch (error) {
+    console.error('Error fetching testimonials:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
--- Frontend/epp_app/src/App.js
+++ Frontend/epp_app/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Testimonials from './Components/Testimonails'; // Assuming Testimonials.js is in the same directory
+// import Footer from './Components/Footer';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      App
+        <Router>
+            <Testimonials />
+        </Router>
     </div>
   );
 }
