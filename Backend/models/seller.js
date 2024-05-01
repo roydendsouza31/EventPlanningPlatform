9f0d627190f331613cf7e1a9ce3d489afb2f08b1
Royden Dsouza <81296538+roydendsouza31@users.noreply.github.com>
2024-05-01 18:32:41 +0530

    updated schema of selller

--- Backend/models/seller.js
+++ Backend/models/seller.js
@@ -5,7 +5,7 @@ const SellerSchema = new mongoose.Schema({
   surname: { type: String, required: true, minlength: 1 },
   email: { type: String, required: true, minlength: 2 },
   password: { type: String, required: true, minlength: 3 },
-  image: { type: String, default: "" },
+  image: { type: Buffer },
   address: { type: String, default: "" },
   phone: { type: String, default: "" },
   typeofservice: { type: String, default: "" },
