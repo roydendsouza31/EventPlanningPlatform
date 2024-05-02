27bf071833d16d92fb06c8594e16a2818e060d22
Royden Dsouza <81296538+roydendsouza31@users.noreply.github.com>
2024-05-02 07:55:37 +0530

    created about page and customer profile page

--- Frontend/epp_app/src/components/customerHomepage/customerHomepage.js
+++ Frontend/epp_app/src/components/customerHomepage/customerHomepage.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./homepage.css";
+import "./customerHomepage.css";
 import { useEffect } from "react";
 import logo from "./assets/logo.png";
 import camera from "./assets/camera.png";
