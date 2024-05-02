8b34a3c19d9db03f79eee005e30deb962d860220
Royden Dsouza <81296538+roydendsouza31@users.noreply.github.com>
2024-05-02 07:34:13 +0530

    updated the homepage footer

--- Frontend/epp_app/src/components/customerHomepage/customerHomepage.js
+++ Frontend/epp_app/src/components/customerHomepage/customerHomepage.js
@@ -200,35 +200,31 @@ const CustomerHomepage = () => {
         <ServiceProvidersPage />
       </div>
 
-      <footer className="footer">
-        <div className="container">
-          <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-            <p className="col-md-4 mb-0 text-muted">
-              © 2021 Creations-Goa, Inc
-            </p>
+      <div class="container">
+        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+          <p class="col-md-4 mb-0 text-muted">© 2021 Creations-Goa, Inc</p>
 
-            <a
-              href="/"
-              className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none"
-            >
-              <svg className="bi me-2" width="40" height="32"></svg>
-            </a>
+          <a
+            href="/"
+            class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none"
+          >
+            <svg class="bi me-2" width="40" height="32"></svg>
+          </a>
 
-            <ul className="nav col-md-4 justify-content-end">
-              <li className="nav-item">
-                <a href="/" className="nav-link px-2 text-muted">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="/about" className="nav-link px-2 text-muted">
-                  About
-                </a>
-              </li>
-            </ul>
-          </footer>
-        </div>
-      </footer>
+          <ul class="nav col-md-4 justify-content-end">
+            <li class="nav-item">
+              <a href="/" class="nav-link px-2 text-muted">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="/about" class="nav-link px-2 text-muted">
+                About
+              </a>
+            </li>
+          </ul>
+        </footer>
+      </div>
     </div>
   );
 };
